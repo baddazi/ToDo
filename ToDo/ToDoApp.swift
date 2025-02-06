918fa5bcdb4c0fe51fbbb1462b512a93b2b00bc4
baddazi <48481706+baddazi@users.noreply.github.com>
2025-02-06 16:57:42 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by David Záruba on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by David Záruba on 06.02.2025.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
