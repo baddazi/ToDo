31a32b44b98802a322fe9697856f79841dad4fc8
baddazi <48481706+baddazi@users.noreply.github.com>
2025-02-06 19:31:10 +0100

    Adding firebse, setup views

---
+++ ToDo/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  ToDo
+//
+//  Created by David Záruba on 06.02.2025.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
